cbbf68f61ec3bd28bfb75064b7482e20225bea76
NotALizard <14036158+NotALizard@users.noreply.github.com>
2025-06-09 15:12:46 -0500

    Fix Dealer's Choice dead on not triggering. Add Lone Ranger card

--- src/main/java/tisHermitBooster/cards/LoneRanger.java
+++ src/main/java/tisHermitBooster/cards/LoneRanger.java
@@ -1,55 +1,68 @@
 package tisHermitBooster.cards;
 
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndAddToHandEffect;
 import hermit.cards.AbstractDynamicCard;
 import hermit.cards.AbstractHermitCard;
 import hermit.characters.hermit;
 import hermit.powers.Rugged;
 import spireTogether.network.P2P.P2PPlayer;
 import spireTogether.network.objects.items.NetworkCard;
-import tisHermitBooster.actions.DealersChoiceAction;
+import tisCardPack.actions.ApplyTauntAction;
+import tisHermitBooster.powers.LoneRangerPower;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 import static tisHermitBooster.tisHermitBoosterMod.cardPath;
 import static tisHermitBooster.tisHermitBoosterMod.makeID;
 
-public class DealersChoice extends AbstractHermitMultiplayerCard {
-    public static final String ID = makeID(DealersChoice.class.getSimpleName());
+public class LoneRanger extends AbstractHermitMultiplayerCard {
+    public static final String ID = makeID(LoneRanger.class.getSimpleName());
     public static final String IMG = cardPath("skill/default.png");
     private static final CardRarity RARITY;
     private static final CardTarget TARGET;
     private static final CardType TYPE;
     public static final CardColor COLOR;
-    private static final int COST = -1;
+    private static final int COST = 2;
+    private static final int MAGIC = 3;
+    private static final int UPGRADE_MAGIC = 2;
 
     static {
-        RARITY = CardRarity.RARE;
-        TARGET = CardTarget.SELF;
+        RARITY = CardRarity.UNCOMMON;
+        TARGET = CardTarget.ENEMY;
         TYPE = CardType.SKILL;
         COLOR = hermit.Enums.COLOR_YELLOW;
     }
 
-    public DealersChoice() {
+    public LoneRanger() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         this.exhaust = true;
+        this.baseMagicNumber = this.magicNumber = MAGIC;
         this.tags.add(AbstractHermitCard.Enums.DEADON);
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new DealersChoiceAction(p,this.upgraded,this.freeToPlayOnce,this.energyOnUse,this.getPlayers(true, true)));
+        addToBot(new ApplyTauntAction(m, p, magicNumber));
+        if(p.hasPower(LoneRangerPower.POWER_ID)){
+            LoneRangerPower lr = (LoneRangerPower)p.getPower(LoneRangerPower.POWER_ID);
+            lr.addTarget(m);
+        }
+        else{
+            addToBot(new ApplyPowerAction(p, p, new LoneRangerPower(p, m)));
+        }
         if (this.isDeadOn()) {
             this.TriggerDeadOnEffect(p, m);
         }
     }
 
     public void DeadOnEffect(AbstractPlayer p, AbstractMonster m) {
-        for(P2PPlayer player : this.getPlayers(true, true)) {
-            player.gainEnergy(1);
-        }
+        addToBot(new ApplyPowerAction(p, p, new Rugged(p, 1)));
     }
 
     public void triggerOnGlowCheck() {
@@ -62,7 +75,7 @@ public class DealersChoice extends AbstractHermitMultiplayerCard {
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.rawDescription = cardStrings.UPGRADE_DESCRIPTION;
+            this.upgradeMagicNumber(UPGRADE_MAGIC);
             this.initializeDescription();
         }
     }
---
+++ src/main/java/tisHermitBooster/powers/LoneRangerPower.java
@@ -0,0 +1,49 @@
+package tisHermitBooster.powers;
+
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import tisCardPack.powers.TauntPower;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+import static tisHermitBooster.tisHermitBoosterMod.makeID;
+
+public class LoneRangerPower extends BasePower {
+    public static final String POWER_ID = makeID(LoneRangerPower.class.getSimpleName());
+    private static final PowerType TYPE = PowerType.BUFF;
+    private static final boolean TURN_BASED = false;
+    private ArrayList<AbstractMonster> targets;
+
+    public LoneRangerPower(AbstractCreature owner, AbstractMonster target) {
+        super(POWER_ID, TYPE, TURN_BASED, owner, -1);
+        this.targets = new ArrayList<>();
+        targets.add(target);
+    }
+
+    public void addTarget(AbstractMonster target){
+        if(!this.targets.contains(target)){
+            this.targets.add(target);
+        }
+    }
+
+    public void onEnergyRecharge() {
+        //For each monster, remove taunt only if we are the one taunting them.
+        for(AbstractMonster m : targets){
+            if(!m.isDead){
+                TauntPower tp = (TauntPower)m.getPower(TauntPower.POWER_ID);
+                if(tp != null){
+                    if (Objects.equals(tp.source, this.owner)) {
+                        this.addToBot(new RemoveSpecificPowerAction(m, this.source, TauntPower.POWER_ID));
+                    }
+                }
+            }
+        }
+        this.addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, POWER_ID));
+    }
+
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0];
+    }
+}
